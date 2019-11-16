7f18b32af70050ebf13f899b55797d0f90ce4713
albertbrigi123 <albertbrigi95@gmail.com>
2019-11-16 19:00:27 +0200

    check data entered in register activity

--- app/src/main/java/com/example/xchange/RegisterActivity.java
+++ app/src/main/java/com/example/xchange/RegisterActivity.java
@@ -3,7 +3,9 @@ package com.example.xchange;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
+
 public class RegisterActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getCanonicalName();
 
@@ -64,11 +68,66 @@ public class RegisterActivity extends AppCompatActivity {
             passwordS=password.getText().toString();
             birthdateS=birthDateView.getText().toString();
             db.insertUser(firstnameS,lastnameS,emailS,passwordS,birthdateS);
-            Intent profintent=new Intent(RegisterActivity.this,ProfileActivity.class);
-            startActivity(profintent);
+            if(checkDataEntered()==true) {
+                Intent profintent = new Intent(RegisterActivity.this, ProfileActivity.class);
+                startActivity(profintent);
+            }
         }
     });
 
     }
 
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    boolean isEmptyT(TextView text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+
+    boolean checkDataEntered() {
+        boolean isTrue=true;
+        if (isEmpty(fname)) {
+            fname.setError("First name is required!");
+            isTrue=false;
+        }
+
+        if (isEmpty(lname)) {
+            lname.setError("Last name is required!");
+            isTrue=false;
+        }
+
+        if (isEmail(email) == false) {
+            email.setError("Enter valid email!");
+            isTrue=false;
+        }
+
+        if (isEmpty(password)) {
+            password.setError("Password is required");
+            isTrue=false;
+        }
+        if (isEmpty(confpassword)) {
+            confpassword.setError("Confirm password is required");
+            isTrue=false;
+        }
+        if (isEmptyT(birthDateView)) {
+            datePickerButton.setError("Birth date is required");
+            isTrue=false;
+        }
+
+        if(confpassword!=password)
+        {
+            confpassword.setError("The confirm password does not match the password");
+            isTrue=false;
+        }
+        return isTrue;
+    }
 }
