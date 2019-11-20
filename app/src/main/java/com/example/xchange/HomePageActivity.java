7add74308671c63b23443d563f733c0d5654f02b
norbertcsiki <norbertcsiki980129@yahoo.com>
2019-11-20 08:08:58 +0200

    a

--- app/src/main/java/com/example/xchange/HomePageActivity.java
+++ app/src/main/java/com/example/xchange/HomePageActivity.java
@@ -3,12 +3,23 @@ package com.example.xchange;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.util.Currency;
+import java.util.Locale;
 
 public class HomePageActivity extends AppCompatActivity {
 
+
+    EditText actEur;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        actEur = findViewById(R.id.actualeuro);
+        //String euro = Currency.getInstance(Locale.GERMANY).getCurrencyCode();
+        //actEur.setText(euro);
+
     }
 }
