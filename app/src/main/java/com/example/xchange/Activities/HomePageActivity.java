f70e8244a29102e59e2499e94db200b00fba092e
norbertcsiki <norbertcsiki980129@yahoo.com>
2019-11-26 17:42:54 +0200

    Homepage. Generate the currency euro value in Json (Need Design)

--- app/src/main/java/com/example/xchange/Activities/HomePageActivity.java
+++ app/src/main/java/com/example/xchange/Activities/HomePageActivity.java
@@ -4,24 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Currency;
 import java.util.Locale;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.xchange.R;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomePageActivity extends AppCompatActivity {
 
 
-    EditText actEur;
+    private TextView resultTextview, actualEuro;
+    private RequestQueue mQueue;
+    Button readButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        actEur = findViewById(R.id.actualeuro);
-        //String euro = Currency.getInstance(Locale.GERMANY).getCurrencyCode();
-        //actEur.setText(euro);
 
+
+        resultTextview = findViewById(R.id.actualeuro);
+        readButton = findViewById(R.id.readbutton);
+        actualEuro = findViewById(R.id.actualeuro);
+        mQueue = Volley.newRequestQueue(this);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "button clicked", Toast.LENGTH_SHORT).show();
+                readJson();
+
+            }
+        });
+
+    }
+
+    private void readJson() {
+        final String url = "https://api.exchangeratesapi.io/latest";
+        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+
+                    Log.d("aaa",response.toString());
+                    JSONObject object = response.getJSONObject("rates");
+                    String value = object.getString("RON");
+                    actualEuro.setText(value);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 }
